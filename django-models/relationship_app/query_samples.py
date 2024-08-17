035784954e203165042d7b907201bff9e416c4e1
Charles Agoe <agoetee@gmail.com>
2024-08-17 23:07:36 +0000

    add relationship app querries

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,14 +1,37 @@
-# >>> chinua = Author.objects.create(name="Chinua Achebe")
-# >>> chinua.save()
-# >>> ama = Author.objects.create(name="Ama Ata Aidoo")
-# >>> ama.save()
-# >>> things = Book.objects.create(title="Things Fall Apart", author=1)
-# things = Book.objects.create(title="Things Fall Apart", author=chinua) 
-# >>> dilemma = Book.objects.create(title="Dilemma of a Ghost", author=ama) 
-# >>> things.save()
-# >>> dilemma.save()
-# >>> Book.objects.get(author=ama) 
-# <Book: Dilemma of a Ghost>
-# >>> Book.objects.get(author=chinua) 
-# <Book: Things Fall Apart>
-# >>>
+from .models import Author, Book, Library, Librarian
+
+# Add Author objects
+chinua = Author.objects.create(name="Chinua Achebe")
+chinua.save()
+ama = Author.objects.create(name="Ama Ata Aidoo")
+ama.save()
+
+#Add Book objects
+things = Book.objects.create(title="Things Fall Apart", author=chinua) 
+dilemma = Book.objects.create(title="Dilemma of a Ghost", author=ama) 
+things.save()
+dilemma.save()
+
+# Get books by one author
+Book.objects.get(author=ama) 
+Book.objects.get(author=chinua) 
+
+# List all books
+Book.objects.all()
+
+# Create Library Instance
+balme = Library.objects.create(name='balme')
+balme.save()
+
+# Add books to Library
+balme.books.add(things, dilemma)
+
+# List all books in Library
+balme.books.all()
+
+# Add Librarian
+agyim = Librarian.objects.create(name='Agyim Taala', library=balme)
+agyim.save()
+
+#Retrieve a Libryrian for a library
+agyim.library
