aa13256d88fa6999c926c08f878d5af2c8f98d20
Charles Agoe <agoetee@gmail.com>
2024-08-18 15:28:46 +0000

    add class based views

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import list_all_books
+from .views import list_all_books, LibraryBooks
 
 urlpatterns = [
     path('books/', list_all_books, name='list_all_books'),
+    path('library/<int:pk>/', LibraryBooks.as_view(), name='library_books'),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,10 +1,23 @@
+from typing import Any
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Book
+from django.views.generic import DetailView
+from .models import Book, Library
 
 # Create your views here.
 def list_all_books(request):
     items = Book.objects.all()
     book_list = [f"{i.title} by {i.author}" for i in items]
-    print(book_list)
     return render(request, "relationship_app/list_books.html", {"book_list": book_list})
+
+
+class LibraryBooks(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+    def get_context_data(self, **kwargs: Any):
+        context = super().get_context_data(**kwargs)
+        context['library'] = self.object
+        return context
+
