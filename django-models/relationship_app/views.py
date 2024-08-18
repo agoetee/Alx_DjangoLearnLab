187ca16d1499f4755a96f007975ea4584b5a8bd0
Charles Agoe <agoetee@gmail.com>
2024-08-18 13:31:11 +0000

    add views function for books

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book
 
 # Create your views here.
+def list_all_books(request):
+    items = Book.objects.all()
+    book_list = [f"{i.title} by {i.author}" for i in items]
+    return HttpResponse(f'{book_list}')
