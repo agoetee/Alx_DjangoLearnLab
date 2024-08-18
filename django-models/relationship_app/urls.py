862cdf49e4a74d1bccc3de3eda9d201bf1a514de
Charles Agoe <agoetee@gmail.com>
2024-08-18 23:31:49 +0000

    add auth import login and logout

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView
-from .views import list_books, LibraryDetailView, register, LoginView, index
+from .views import list_books, LibraryDetailView, LoginView, index
+from . import views
 
 urlpatterns = [
     path('books/', list_books, name='list_all_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_books'),
-    path('register/', register, name='register'),
-    path('login/', LoginView.as_view(next_page='books')),
-    path('logout/', LogoutView.as_view(next_page='/')),
+    path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html')),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html')),
     path('', index, name='index'),
 ]
