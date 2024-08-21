80d584e058e32e3c8126f7fb20595bfda3d4169d
Charles Agoe <agoetee@gmail.com>
2024-08-21 23:25:48 +0000

    update url with edit_book/

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -20,6 +20,6 @@ urlpatterns = [
     path('member_view/', member_view, name='member_view'),
 
     path('add_book/', add_book, name='add_book'),
-    path('change_book/<int:book_id>/', change_book, name='change_book'),
+    path('edit_book/<int:book_id>/', change_book, name='change_book'),
     path('delete_book/<int:book_id>/', delete_book, name='delete_book'),
 ]
