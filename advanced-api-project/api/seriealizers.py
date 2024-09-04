c302eacfd83312729cfa7f43e6a7b8712f6b1d2d
Charles Agoe <agoetee@gmail.com>
2024-09-04 21:39:05 +0000

    capitalize Validation in ValidationError

--- advanced-api-project/api/seriealizers.py
+++ advanced-api-project/api/seriealizers.py
@@ -12,11 +12,11 @@ class BookSerializer(serializers.ModelSerializer):
         model = Book
         fields = ['title', 'publication_year', 'author']
 
-    def validate_year(self, data):
+    def validate_year(self, value):
         current_year = datetime.date.today().year
-        if data['publication_year'] > current_year:
-            raise serializers.validationError(f'Publication year cannot be in the future. Current year: {current_year}')
-        return data
+        if value > current_year:
+            raise serializers.ValidationError(f'Publication year cannot be in the future. Current year: {current_year}')
+        return value
 
 
 purple= Book(title="Purple Hibiscus", publication_year='2020', author=chima)
