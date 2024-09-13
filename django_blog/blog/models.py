982004982c04db75bf2c33edf98ba69e2771f1c8
Charles Agoe <agoetee@gmail.com>
2024-09-13 19:57:40 +0000

    add authentication user views

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,8 +1,29 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser, UserManager
 
-class Author(User):
-    """"""
+class CustomUserManager(UserManager):
+    def create_user(self, email, password):
+        if not email:
+            raise ValueError("Email is reqiored")
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True, max_length=255)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django import forms
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
 
-# Create your views here.
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField()
+    
+    class Meta:
+        model = CustomUser
+        fields = ['email',]
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = RegistrationForm()
+    return render(request, 'register.html', {'form': form})
+    
+class LoginView(LoginView):
+    template_name = 'login.html'
