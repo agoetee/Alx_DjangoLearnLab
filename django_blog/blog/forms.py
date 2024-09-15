eaa54ec136b35fe2f40cdf34ec9b98a0cd0e3a7d
Charles Agoe <agoetee@gmail.com>
2024-09-15 23:56:31 +0000

    add tagging fields in tag form

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-from .models import Profile, Post, Comment
+from taggit.forms import TagWidget
+from taggit.utils import slugify
+from .models import Profile, Post, Comment, Tag
 
 class ProfileForm(forms.ModelForm):
     class Meta:
@@ -7,9 +9,20 @@ class ProfileForm(forms.ModelForm):
         fields = ('email', 'bio', 'picture')
 
 class CreatePostForm(forms.ModelForm):
+    tags = forms.CharField(required=False, widget=TagWidget)
+
     class Meta:
         model = Post
         fields = ('title', 'content')
+        widgets = {
+            'tags': TagWidget
+        }
+
+    def clean_tags(self):
+        tags = self.cleaned_data['tags']
+        existing_tags = Tag.objects.filter(name__in=[slugify(tags) for tag in tags.split(',')])
+        new_tags = [tag for tag in tags.split(',') if slugify(tag) not in existing_tags.values_list('name', flat=True)]
+        return ', '.join(existing_tags.values_list('name', flat=True) + new_tags)
 
 class UpdatePostForm(forms.ModelForm):
     class Meta:
