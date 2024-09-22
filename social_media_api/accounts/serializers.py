0735560d6258f5e2e2cbdea2d99beb1e8e8e7112
Charles Agoe <agoetee@gmail.com>
2024-09-22 23:49:17 +0000

    success: account creation

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,4 +1,8 @@
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
 from .models import CustomUser
 
 class CustomUserSerializer(serializers.ModelSerializer):
@@ -9,15 +13,42 @@ class CustomUserSerializer(serializers.ModelSerializer):
         model = CustomUser
         fields = ['username', 'email', 'bio', 'profile_pic', 'followers', 'following']
 
+    
+    
+class RegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(min_length=8, write_only=True)
+    confirm_password = serializers.CharField(min_length=8, write_only=True)
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'email', 'password', 'confirm_password']
+
+    def validate(self, data):
+        if data['password'] != data['confirm_password']:
+            raise serializers.ValidationError({'confirm_password': "Password mismatch"})
+        return data
+        
     def create(self, validated_data):
-        user = CustomUser.objects.create(
-            username=validated_data['username'],
-            email = validated_data['email']
-        )
-        user.set_password(validated_data['password'])
-        user.save()
-        return user
+        user_data = {
+            'username' : validated_data['username'],
+            'email' : validated_data['email'],
+            'password' : validated_data['password'],
+        }
+        return get_user_model().objects.create_user(**user_data)
+
     
 class UserLoginSerializer(serializers.Serializer):
     username = serializers.CharField(required=True)
     password = serializers.CharField(required=True, write_only=True)
+    token = serializers.CharField(max_length=255, read_only=True)
+
+    def validate(self, data):
+        username = data.get('username')
+        password = data.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user is None:
+            raise serializers.ValidationError({'detail': "Invalid Credentials"})
+        token, created = Token.objects.create(user=user)
+        return Response({'username': user.username, 'token': token.key})
+    
