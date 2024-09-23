9abe370343205b4c7c3b3deaeeac60bb64701b62
Charles Agoe <agoetee@gmail.com>
2024-09-23 03:35:57 +0000

    remove restriction from textfield in Post

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -4,7 +4,7 @@ from django.conf import settings
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=150)
-    content = models.TextField(max_length=500)
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
