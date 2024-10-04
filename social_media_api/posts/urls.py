86a9a849c29bb8a81d1a63ce3346e9e20f270cbe
Charles Agoe <agoetee@gmail.com>
2024-10-04 11:49:59 +0000

    update user model with following

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from .views import PostList, PostDetail, PostBlog,listPost
+from .views import PostList, PostDetail
 
 urlpatterns = [
     path('posts/', PostList.as_view(), name='posts'),
     path('posts/<int:pk>/', PostDetail.as_view(), name='post_detail'),
-    path('post/', PostBlog, name="post"),
-    path('getposts/', listPost, name="post")
 ]
